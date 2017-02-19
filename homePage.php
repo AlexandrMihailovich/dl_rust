aa9bb5f4dec266ce1ad44de82b9276aabb40bfa8
DrFr <aleksander.saler@gmail.com>
2017-02-19 12:47:45 +0300

    add

--- homePage.php
+++ homePage.php
@@ -410,12 +410,18 @@
     <div class="container">
 
         <div class="row">
+            <div class="col-md-5"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-3"></div>
         </div>
 
 
         <div class="row">
+            <div class="col-md-8"></div>
+            <div class="col-md-8"></div>
         </div>
-        
+
     </div>
 </div>
 
