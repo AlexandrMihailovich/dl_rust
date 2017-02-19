8131a320d9c7a8f1dac174c82503b9ccbf7af634
DrFr <aleksander.saler@gmail.com>
2017-02-19 10:31:58 +0300

    add

--- homePage.php
+++ homePage.php
@@ -1,9 +1,103 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,
+                                   minimum-scale=1,
+                                   maximum-scale=1,
+                                   initial-scale=1,
+                                   user-scalable=no" >
+
+    <title>dust-home</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
 <body>
+
+<header class="container">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">_1981_</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <form class="navbar-form navbar-right">
+                    <div class="form-group">
+                        <div class="form-control">
+                            <span class="user-greeting">Welcome Back </span>
+                            <span class="user-name">John Doe81</span>
+                        </div>
+                        <?php /*<input type="text" class="form-control" placeholder="Search">*/?>
+                    </div>
+                    <button type="submit" class="btn btn-default">Client Sing-Out</button>
+                </form>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
+
+
+<div class="featured-image">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-16">
+                <img src="img/featured-img.png" class="img-responsive center-block">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<section class="featured">
+    <div class="container">
+        <div class="text-center">
+            <h2>Featured Title</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-2"><img src="img/Layer-3.png" alt="..."></div>
+            <div class="col-md-2"><img src="img/Layer-3-copy-2.png" alt="..."></div>
+            <div class="col-md-2"><img src="img/Layer-3-copy-3.png" alt="..."></div>
+            <div class="col-md-9">
+                <header><div class="media">
+                        <div class="pull-left" href="#">
+                            <img class="media-object" src="img/Cert.png" alt="...">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">Krili havtret hyo gustripast</h4>
+                            <em>Krili havtret hyo gustripast</em>
+                        </div>
+                    </div>
+                </header>
+                <div class="feature-content">
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 </body>
 </html>
